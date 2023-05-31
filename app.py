0a179f32994bccb5e047185a201a13702170dd4a
jovanpejic <42420344+jovanpejic@users.noreply.github.com>
2023-05-31 22:00:49 +0200

    final

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
+
+
 
 
 def main():
@@ -44,10 +45,7 @@ def main():
         
         llm = OpenAI()
         chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
-           
+        response = chain.run(input_documents=docs, question=user_question)
         st.write(response)
     
 
